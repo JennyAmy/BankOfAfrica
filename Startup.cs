55fb3970992c9b21e773bc9b377469e56db8aafe
Jennifer Oliseyenum <jennifer.oliseyenum@fintraksoftware.com>
2022-06-19 18:32:44 -0700

    Swagger documentation modified

--- Startup.cs
+++ Startup.cs
@@ -60,17 +60,30 @@ namespace BankOfAfricaAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BankOfAfricaAPI", Version = "v1" });
-                
-                // To Enable authorization using Swagger (JWT)
-                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+
+                var securitySchema = new OpenApiSecurityScheme
                 {
+                    Description = "JWT Authorization header using the Bearer scheme. Just paste the generated token in the value input box",
                     Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
                     In = ParameterLocation.Header,
-                    Description = "Enter 'Bearer' [space] and then your valid token in the text input below.\r\n\r\nExample: \"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\"",
-                });
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+
+                c.AddSecurityDefinition("Bearer", securitySchema);
+
+                var securityRequirement = new OpenApiSecurityRequirement
+                {
+                    { securitySchema, new[] { "Bearer" } }
+                };
+
+                c.AddSecurityRequirement(securityRequirement);
+
             });
         }
 
